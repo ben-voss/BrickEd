248c685f0b8ade6c4a5723f9ac94047a2ccce205
Ben Voss <ben.voss@me.com>
2021-07-23 23:06:05 +0100

    Delete incorrectly named file

--- src/app/partTree/primitive.ts
+++
@@ -1,7 +0,0 @@
-import Polygon from "../bsp/Polygon";
-import CsgPolygon from "../csg/Polygon";
-
-export default interface Primitive {
-  collectPolygons(polygons: Polygon[]): void;
-  collectCsgPolygons(polygons: CsgPolygon[]): void;
-}
